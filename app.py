9701de5caa721fd9bb7377597d3e8efb74c987bd
Don86 <dh.teng.86@gmail.com>
2019-01-16 18:10:45 +1100

    date input fields not working; type error

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import dash_core_components as dcc
 import dash_html_components as html
 
 from pandas_datareader import data as web
-from datetime import datetime as dt
+from datetime import datetime as dt, timedelta
 from scipy.signal import savgol_filter
 
 from flask import send_from_directory
@@ -20,15 +20,15 @@ app = dash.Dash(__name__)
 
 app.layout = html.Div([
 	# link to css
-	html.Link(
-        rel='stylesheet',
-        href='/static/style.css'
-    ),
+	#html.Link(
+    #    rel='stylesheet',
+    #    href='/static/style.css'
+    #),
 
 	# Title
 	html.Div(
 		className="app-header",
-		children=[html.Div("Title", className="app-header-title")]
+		children=[html.Div("Index Viz", className="app-header-title")]
 		),
 	
 	# PyDash logo at top right
@@ -42,14 +42,14 @@ app.layout = html.Div([
 		{"label":"Tesla", "value":"TSLA"}],
 		value="^GSPC"),
 
-	# Date picker
+	# ========== Date pickers: to and from ==========
 	dcc.DatePickerSingle(
 		id="from-date",
 		min_date_allowed=dt(1997, 1, 1),
 		max_date_allowed=dt.now(),
 		month_format='MMM Do, YY',
 		placeholder='MMM Do, YY',
-		date=dt.now()
+		date=dt.now() - timedelta(days=7)
 		),
 
 	dcc.DatePickerSingle(
@@ -66,17 +66,26 @@ app.layout = html.Div([
 	])
 
 
-@app.callback(Output("my-graph", "figure"), [Input("my-dropdown", "value")])
-def update_graph(selected_dropdown_value):
+@app.callback(Output("my-graph", "figure"), 
+	[Input("my-dropdown", "value"), 
+	Input("from-date", "date"),
+	Input("to-date", "date"),
+	])
+def update_graph(selected_dropdown_value, from_date, to_date):
 	"""Updates graph according to selected ticker symbol.
 	"""
 	df = web.DataReader(selected_dropdown_value, 
 		data_source="yahoo",
-		start=dt(1997,1,1), 
-		end=dt.now())
+		#start=dt(1997,1,1), 
+		start=from_date,
+		#end=dt.now())
+		end=to_date)
 
 	# Compute column of smoothed values
-	df["smoothed"] = savgol_filter(list(df["Close"]), 61, 3)
+	#d1 = dt.strptime(from_date, "%Y-%m-%d")
+	#d2 = dt.strptime(to_date, "%Y-%m-%d")
+	window = min((to_date - from_date).days, 61)
+	df["smoothed"] = savgol_filter(list(df["Close"]), window, 3)
 
 	return {"data":[{"x": df.index, "y": df.Close, "type": "markers", "name": "close_val", 
 	"line": dict(color = ('rgb(0, 76, 153)'), width=0.5)}, 
